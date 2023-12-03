ebc7752957cf72ce5de20ce89c1b67461d4ca06c
TechGamingGeekCoding <109979523+prohackergamer3@users.noreply.github.com>
2023-12-03 17:23:36 +0300

    main
    main

---
+++ ipw.c
@@ -0,0 +1,14 @@
+//Install python without error
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+system("pkg install python");
+system("pkg install python3");
+system("apt install python");
+system("apt install python3");
+system("pkg install openssl");
+system("apt install openssl");
+printf("✓\n");
+
+
+}
